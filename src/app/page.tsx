00825b57443ee0603a6feec2ef2482d6dde9d745
gracemonzales <galanagamarygrace@gmail.com>
2024-07-14 01:30:10 +0800

    chore: Add NextUIProvider at root app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { NextUIProvider } from "@nextui-org/system";
 import { Typography } from "src/components/base";
 import Container from "src/components/feature/Container";
 import MainBanner from "src/components/feature/MainBanner";
@@ -7,25 +8,27 @@ export default function Home() {
   const socials = ["Github", "Gitlab", "LinkedIn", "Twitter"] as SocialsEnum[];
 
   return (
-    <Container>
-      {/* floating SNS */}
-      <div className="flex h-full">
-        <div className="hidden lg:block max-w-80 w-80 min-w-40">
-          <div className="h-full flex flex-col justify-between">
-            <div className="flex flex-col justify-start items-center">
-              <div className="w-px h-20 mb-32 bg-flamingo"></div>
-              <div className="-rotate-90">
-                <Typography.BodyText className="text-flamingo">
-                  galanagamarygrace@gmail.com
-                </Typography.BodyText>
+    <NextUIProvider>
+      <Container>
+        {/* floating SNS */}
+        <div className="flex h-full">
+          <div className="hidden lg:block max-w-80 w-80 min-w-40">
+            <div className="h-full flex flex-col justify-between">
+              <div className="flex flex-col justify-start items-center">
+                <div className="w-px h-20 mb-32 bg-flamingo"></div>
+                <div className="-rotate-90">
+                  <Typography.BodyText className="text-flamingo">
+                    galanagamarygrace@gmail.com
+                  </Typography.BodyText>
+                </div>
               </div>
+              <SocialList platforms={socials} />
             </div>
-            <SocialList platforms={socials} />
           </div>
+          <MainBanner></MainBanner>
+          <div className="hidden lg:block max-w-80 w-80 min-w-40"></div>
         </div>
-        <MainBanner></MainBanner>
-        <div className="hidden lg:block max-w-80 w-80 min-w-40"></div>
-      </div>
-    </Container>
+      </Container>
+    </NextUIProvider>
   );
 }
