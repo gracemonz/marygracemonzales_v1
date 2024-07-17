724bccaf584b09774d231a375f41838ad36a75d5
gracemonzales <galanagamarygrace@gmail.com>
2024-07-17 23:23:13 +0800

    chore: fix white space on main HTML skeleton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-twilight">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className={`bg-twilight ${inter.className}`}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
